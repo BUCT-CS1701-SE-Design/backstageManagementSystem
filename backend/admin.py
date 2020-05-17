429df3747489b30afb3dd7417d8cc3d3336c3938
name <buctjike1701@163.com>
2020-05-17 19:41:47 +0800

    admin注册

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Museum
-admin.site.register(Museum)
+import backend.models as mdl
+admin.site.register(mdl.Museum)
+admin.site.register(mdl.Academic)
+admin.site.register(mdl.Exhibition)
+admin.site.register(mdl.Explanation)
+admin.site.register(mdl.Users)
+admin.site.register(mdl.Education)
+admin.site.register(mdl.Collection)
+admin.site.register(mdl.Museumnews)
+admin.site.register(mdl.Usercomments)
 
 
 # Register your models here.
