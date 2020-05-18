5574d8544eaa4aa1f1413ada2dca7925ebf97992
name <buctjike1701@163.com>
2020-05-18 21:11:06 +0800

    更新了数据表

--- backend/models.py
+++ backend/models.py
@@ -20,6 +20,72 @@ class Academic(models.Model):
         db_table = 'academic'
 
 
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
 class Collection(models.Model):
     collectionid = models.AutoField(db_column='CollectionID', primary_key=True)  # Field name made lowercase.
     museumid = models.ForeignKey('Museum', models.DO_NOTHING, db_column='museumID')  # Field name made lowercase.
@@ -27,10 +93,55 @@ class Collection(models.Model):
     collectionintroduction = models.TextField(db_column='collectionIntroduction', blank=True, null=True)  # Field name made lowercase.
     collection_age = models.TextField(blank=True, null=True)
     collectionimage = models.TextField(db_column='collectionImage', blank=True, null=True)  # Field name made lowercase.
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
         db_table = 'collection'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Education(models.Model):
@@ -38,6 +149,7 @@ class Education(models.Model):
     museumid = models.ForeignKey('Museum', models.DO_NOTHING, db_column='museumID')  # Field name made lowercase.
     educationname = models.CharField(db_column='educationName', max_length=45)  # Field name made lowercase.
     educationlink = models.CharField(db_column='educationLink', max_length=45)  # Field name made lowercase.
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
@@ -54,6 +166,7 @@ class Exhibition(models.Model):
     exhibitiontel = models.TextField(db_column='exhibitionTel', blank=True, null=True)  # Field name made lowercase.
     exhibitionthinglist = models.TextField(db_column='exhibitionThingList', blank=True, null=True)  # Field name made lowercase.
     exhibition_picture = models.TextField(blank=True, null=True)
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
@@ -67,6 +180,7 @@ class Explanation(models.Model):
     explanationtime = models.IntegerField(db_column='explanationTime')  # Field name made lowercase.
     explanationlanguage = models.TextField(db_column='explanationLanguage')  # Field name made lowercase.
     recommendedtime = models.FloatField(db_column='recommendedTime')  # Field name made lowercase.
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
@@ -89,6 +203,8 @@ class Museum(models.Model):
     admission_fee = models.TextField(blank=True, null=True)
     building_time = models.CharField(max_length=45, blank=True, null=True)
     collection_number = models.IntegerField(blank=True, null=True)
+    city = models.TextField(blank=True, null=True)
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
@@ -102,6 +218,7 @@ class Museumnews(models.Model):
     newstime = models.DateField(db_column='newsTime')  # Field name made lowercase.
     newsmaintext = models.TextField()
     positive_negative = models.CharField(db_column='positive/negative', max_length=45)  # Field renamed to remove unsuitable characters.
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
@@ -127,6 +244,8 @@ class Usercomments(models.Model):
     sentianalysis_environment = models.PositiveIntegerField(db_column='sentiAnalysis_environment')  # Field name made lowercase.
     sentianalysis_exhibit = models.IntegerField(db_column='sentiAnalysis_exhibit')  # Field name made lowercase.
     sentianalysis_service = models.IntegerField(db_column='sentiAnalysis_service')  # Field name made lowercase.
+    commentdate = models.DateTimeField()
+    status = models.PositiveIntegerField()
 
     class Meta:
         managed = False
