03dfe996b6bfa506420782d1b0340f2279c2a70e
name <buctjike1701@163.com>
2020-05-20 22:49:03 +0800

    用户评论界面修改

--- frontend/src/views/comments/index.vue
+++ frontend/src/views/comments/index.vue
@@ -17,30 +17,40 @@
           {{ scope.$index }}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column label="评论内容">
         <template slot-scope="scope">
-          {{ scope.row.title }}
+          {{ scope.row.fields.comment }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="评论者ID" width="110" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          <span>{{ scope.row.fields.userid }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="环境评分" width="50" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          {{ scope.row.fields.sentianalysis_environment }}
+        </template>
+      </el-table-column>
+      <el-table-column label="展览评分" width="50" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.fields.sentianalysis_exhibit }}
+        </template>
+      </el-table-column>
+      <el-table-column label="服务评分" width="50" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.fields.sentianalysis_service }}
         </template>
       </el-table-column>
       <el-table-column class-name="status-col" label="Status" width="110" align="center">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+          <el-tag :type="scope.row.fields.status | statusFilter">{{ scope.row.fields.status }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column align="center" prop="created_at" label="评论时间" width="200">
         <template slot-scope="scope">
           <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <span>{{ scope.row.fields.commentdate }}</span>
         </template>
       </el-table-column>
     </el-table>
