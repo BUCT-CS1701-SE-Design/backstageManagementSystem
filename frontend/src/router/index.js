a14393231530e66c3f925ae86a86e36b94a9f98e
name <buctjike1701@163.com>
2020-05-21 21:37:43 +0800

    修改了导航栏路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -71,55 +71,54 @@ export const constantRoutes = [
     component: Layout,
     redirect: '/datas/museums',
     name: 'datas',
-    meta: {title: '数据管理', icon: 'form' },
+    meta: { title: '数据管理', icon: 'form' },
     children: [
       {
         path: 'museums',
         name: 'museums',
-        component: ()=> import("@/views/museums/index"),
-        meta: {title: '博物馆信息'}
+        component: () => import('@/views/museums/index'),
+        meta: { title: '博物馆信息' }
       },
       {
         path: 'exhibitions',
         name: 'exhibitions',
-        component: ()=> import("@/views/exhibitions/index"),
-        meta: {title: '展览信息'}
+        component: () => import('@/views/exhibitions/index'),
+        meta: { title: '展览信息' }
       },
       {
         path: 'collections',
         name: 'collections',
-        component: ()=> import("@/views/collections/index"),
-        meta: {title: '藏品信息'}
+        component: () => import('@/views/collections/index'),
+        meta: { title: '藏品信息' }
       },
       {
         path: 'news',
         name: 'news',
-        component: ()=> import("@/views/news/index"),
-        meta: {title: '新闻信息'}
+        component: () => import('@/views/news/index'),
+        meta: { title: '新闻信息' }
       },
       {
         path: 'explainations',
         name: 'explainations',
-        component: ()=> import("@/views/explainations/index"),
-        meta: {title: '讲解信息'}
+        component: () => import('@/views/explainations/index'),
+        meta: { title: '讲解信息' }
       },
       {
         path: 'comments',
         name: 'comments',
-        component: ()=> import("@/views/comments/index"),
-        meta: {title: '评论信息'}
+        component: () => import('@/views/comments/index'),
+        meta: { title: '评论信息' }
       },
       {
         path: 'sql_log',
         name: 'sql_log',
-        component: ()=> import("@/views/sql_log/index"),
-        meta: {title: '数据库操作日志'}
+        component: () => import('@/views/sql_log/index'),
+        meta: { title: '数据库操作日志' }
       }
-       
+
     ]
   },
   {
-    path: '/example',
     path: '/reviewexplain',
     component: Layout,
     redirect: '/reviewexplain/reviewing',
@@ -161,109 +160,6 @@ export const constantRoutes = [
       }
     ]
   },
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
-
-  // {
-  //   path: '/form',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       name: 'Form',
-  //       component: () => import('@/views/form/index'),
-  //       meta: { title: 'Form', icon: 'form' }
-  //     }
-  //   ]
-  // },
-
-  // {
-  //   path: '/nested',
-  //   component: Layout,
-  //   redirect: '/nested/menu1',
-  //   name: 'Nested',
-  //   meta: {
-  //     title: 'Nested',
-  //     icon: 'nested'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'menu1',
-  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
-  //       name: 'Menu1',
-  //       meta: { title: 'Menu1' },
-  //       children: [
-  //         {
-  //           path: 'menu1-1',
-  //           component: () => import('@/views/nested/menu1/menu1-1'),
-  //           name: 'Menu1-1',
-  //           meta: { title: 'Menu1-1' }
-  //         },
-  //         {
-  //           path: 'menu1-2',
-  //           component: () => import('@/views/nested/menu1/menu1-2'),
-  //           name: 'Menu1-2',
-  //           meta: { title: 'Menu1-2' },
-  //           children: [
-  //             {
-  //               path: 'menu1-2-1',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-  //               name: 'Menu1-2-1',
-  //               meta: { title: 'Menu1-2-1' }
-  //             },
-  //             {
-  //               path: 'menu1-2-2',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-  //               name: 'Menu1-2-2',
-  //               meta: { title: 'Menu1-2-2' }
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           path: 'menu1-3',
-  //           component: () => import('@/views/nested/menu1/menu1-3'),
-  //           name: 'Menu1-3',
-  //           meta: { title: 'Menu1-3' }
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       path: 'menu2',
-  //       component: () => import('@/views/nested/menu2/index'),
-  //       meta: { title: 'menu2' }
-  //     }
-  //   ]
-  // },
-
-  // {
-  //   path: 'external-link',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-  //       meta: { title: 'External Link', icon: 'link' }
-  //     }
-  //   ]
-  // },
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
@@ -277,7 +173,6 @@ const createRouter = () => new Router({
 
 const router = createRouter()
 
-// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
